d4cf39dc7b44bdfe29d6dbac0f902a0db0937ca1
Jada Murray <jada736@icloud.com>
2025-04-16 15:55:11 +0100

    insert method

--- src/main/java/com/mycompany/londonpostcodemanager/binaryTree/BinarySearchTree.java
+++ src/main/java/com/mycompany/londonpostcodemanager/binaryTree/BinarySearchTree.java
@@ -15,7 +15,47 @@ public class BinarySearchTree {
     }
 
     public BinarySearchTree() {
-        root = null;
-        size = 0;
+        this.root = null;
+        this.size = 0;
+    }
+
+    public int count() {
+        return size;
+    }
+
+    public void insert(String postcode) {
+        if (postcode == null) {
+            throw new IllegalArgumentException("postcode cannot be null");
+        }
+        if (root == null) {
+            root = new Node(postcode);
+            this.size ++;
+            return;
+        }
+        Node current = root, parent = null; // starting at the root
+        while (current != null) {
+            parent = current;
+            int cmp = postcode.compareTo(current.postcode);
+            if (cmp == 0) { // postcode to insert is already in the tree
+                System.out.println("Postcode " + postcode + " already exists");
+                return;
+            } else if (cmp < 0) { // if postcode is smaller than the current node, move to the left
+                current = current.left;
+            } else { // if bigger, move to the right
+                current = current.right;
+            }
+        }
+        Node newNode = new Node(postcode); // creating new node and linking it to the tree
+        int cmp = postcode.compareTo(current.postcode);
+        if (cmp < 0) {
+            parent.left = newNode;
+        } else if (cmp > 0) {
+            parent.right = newNode;
+        }
+        this.size++;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 }
